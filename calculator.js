9dc85c022d916b54c87069b1111071d0e91feede
kinkybutt <kadik.becerra@brightsoft.io>
2021-03-16 12:11:15 -0400

    Calculator logic completed.

--- calculator.js
+++ calculator.js
@@ -44,14 +44,55 @@ let inputArr = inputForm.innerHTML;
 })
 
 equal.addEventListener('click', function() {
-    clearInput();
+    // Getting all the numbers
+    let numbers = inputArr.replaceAll(' ', '').split(/\+|\x|\-|\//);
+
+    // Getting all the operators
+    let operators = inputArr.replace(/[0-9]|\.|\s+/g, '').split('');
+
+    // Doing the operations
+
+    // Division
+    let division = operators.indexOf('/');
+    while(division != -1) {
+        numbers.splice(division, 2, numbers[division] / numbers[division + 1]);
+        operators.splice(division, 1);
+        division = operators.indexOf('/');
+    }
+
+    // Multiplication
+    let multiplication = operators.indexOf('x');
+    while(multiplication != -1) {
+        numbers.splice(multiplication, 2, numbers[multiplication] * numbers[multiplication + 1]);
+        operators.splice(multiplication, 1);
+        multiplication = operators.indexOf('x');
+    }
+
+    // Sum
+    let sum = operators.indexOf('+');
+    while(sum != -1) {
+        numbers.splice(sum, 2, numbers[sum] + numbers[sum + 1]);
+        operators.splice(sum, 1);
+        sum = operators.indexOf('+');
+    }
+
+    // Subs
+    let subs = operators.indexOf('-');
+    while(subs != -1) {
+        numbers.splice(subs, 2, numbers[subs] - numbers[subs + 1]);
+        operators.splice(subs, 1);
+        subs = operators.indexOf('-');
+    }
+
+    // Clear the previous input and replace it with the result
+    clearInput(numbers[0]);
 });
 
 resetButton.addEventListener('click', function() {
     clearInput();
 });
 
-function clearInput() {
-    inputForm.innerHTML = '';
-    inputArr = '';
+function clearInput(result = '') {
+    inputForm.innerHTML = result;
+    inputArr = result;
 }
