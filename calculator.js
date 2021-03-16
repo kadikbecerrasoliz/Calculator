c820dd2ee978a31006f9998b5a3fcba5453c3d04
kinkybutt <kadik.becerra@brightsoft.io>
2021-03-15 20:52:01 -0400

    Calculator input test completed

--- calculator.js
+++ calculator.js
@@ -6,15 +6,40 @@ const equalButton = document.querySelector('#equal');
 
 const buttons = document.querySelectorAll('.button');
 
-let inputArr;
+let inputArr = inputForm.innerHTML;
 
 [...buttons].forEach(function(el) {
     el.addEventListener('click', function() {
+        let currentChar = this.innerHTML;
+        let maxPos = inputArr.length - 1;
+        let lastChar = inputArr[maxPos];
+
         if(this.className.includes('operator')) {
-            inputForm.innerHTML = inputForm.innerHTML + ' ' + this.innerHTML + ' ';
-        } else {
-            inputForm.innerHTML += this.innerHTML;
+
+            if(!inputArr) return;
+
+            if(lastChar == '/' || lastChar == 'x' || lastChar == '+' || lastChar == '-') {
+                inputForm.innerHTML = inputArr.substring(0, maxPos) + currentChar + ' ';
+            }
+
+            if(parseInt(lastChar)) {
+                inputForm.innerHTML = inputForm.innerHTML + ' ' + currentChar + ' ';
+            }
         }
+
+        if(this.id == 'dot') {
+            if(!inputArr) return;
+
+            if(lastChar == '/' || lastChar == 'x' || lastChar == '+' || lastChar == '-') return;
+
+            if(lastChar == '.') return;
+
+            inputForm.innerHTML += currentChar;
+        }
+
+        if(this.className == 'button number') inputForm.innerHTML += currentChar;
+
+        inputArr = inputForm.innerHTML.trim();
     });
 })
 
